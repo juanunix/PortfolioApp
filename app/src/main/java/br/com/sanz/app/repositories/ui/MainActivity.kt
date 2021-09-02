1eaa715e9d001ba261949f8795db824f2039f53b
Juan Sanz <juanunix@gmail.com>
2021-09-02 11:07:43 -0300

    base_project

---
+++ app/src/main/java/br/com/sanz/app/repositories/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.sanz.app.repositories.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.sanz.app.repositories.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+}
